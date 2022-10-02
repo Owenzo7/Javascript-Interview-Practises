5bd8bf204a4a435f40e0f08cd14d3440a36e0ce3
Owen Lee <owenlee545@gmail.com>
2022-10-02 20:05:19 +0300

    ReverseString

---
+++ ReverseString/ReverseString.js
@@ -0,0 +1,14 @@
+const reverseString = (str) => {
+
+    let reversed = ""
+
+    for (let i = 0; i < str.length; i++) {
+       
+        reversed = str[i] + reversed
+        
+    }
+
+    return reversed
+}
+
+console.log(reverseString("don"))
