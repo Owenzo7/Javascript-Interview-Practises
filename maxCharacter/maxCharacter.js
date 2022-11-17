e66be20cf3be94ab77366862baba927dbe2b5852
Owen Lee <owenlee545@gmail.com>
2022-11-17 12:15:02 +0300

    done with max char

--- maxCharacter/maxCharacter.js
+++ maxCharacter/maxCharacter.js
@@ -11,24 +11,65 @@
 // 1. Create a Char Map.
 // 2.
 
+// 80K Question.
+
+// const maxCharacter = (str) => {
+//   const charMap = {};
+//   let max = 0;
+//   let maxChar = "";
+
+//   for (let char of str) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+
+//  for(let char in charMap){
+
+//    if (charMap[char] > max){
+//     max= charMap[char]
+//     maxChar = char;
+//    }
+//  }
+
+//  return maxChar
+// };
+
+// console.log(maxCharacter("aaaaabe"));
+
+// Top tech method.
+
+// const str = "simplified !";
+
+// const CharMap = new Map();
+
+// for (const char of str) {
+//   const count = CharMap.get(char);
+
+//   CharMap.set(char, count + 1 || 1);
+// }
+
+// for (const [char, count] of CharMap) {
+//   console.log(char, count);
+// }
+
 const maxCharacter = (str) => {
   const charMap = {};
+
   let max = 0;
+
   let maxChar = "";
 
-  for (let char of str) {
-    charMap[char] = charMap[char] + 1 || 1;
+  for (const char of str) {
+    charMap.set(char, charMap.get(char) + 1 || 1);
   }
 
- for(let char in charMap){
+  for (const [char, count] of charMap) {
+    if (count > max) {
+      max = count;
+      maxChar = char;
+    }
+  }
 
-   if (charMap[char] > max){
-    max= charMap[char]
-    maxChar = char;
-   }
- }
-
- return maxChar
+  return maxChar;
 };
 
-console.log(maxCharacter("aaaaabe"));
+console.log(maxCharacter("gggggghw"));
