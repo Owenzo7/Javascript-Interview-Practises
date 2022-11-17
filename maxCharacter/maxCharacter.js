fc539d8f99319f28f7c100a13b86a7a6a80248e4
Owen Lee <owenlee545@gmail.com>
2022-11-17 11:49:53 +0300

    Max character Method 1 done

--- maxCharacter/maxCharacter.js
+++ maxCharacter/maxCharacter.js
@@ -1,10 +1,34 @@
 // Given a String, find the character that is used the maximum
 // number of times
 
+// Example
+
+// maxCharacter('aabbbccd') === 'b'
+// maxCharacter('foo 111123') === '1'
+
+// ----> Steps <----
+
+// 1. Create a Char Map.
+// 2.
+
 const maxCharacter = (str) => {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
 
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
 
+ for(let char in charMap){
 
-}
+   if (charMap[char] > max){
+    max= charMap[char]
+    maxChar = char;
+   }
+ }
 
-console.log(maxCharacter("aaaabcd"))
+ return maxChar
+};
+
+console.log(maxCharacter("aaaaabe"));
